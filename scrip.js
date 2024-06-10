65b1c2ce151cea37ce432a0377962ae6395604de
ulum234 <mfthululum2323@gmail.com>
2024-06-10 11:26:59 +0700

    cv awal

---
+++ scrip.js
@@ -0,0 +1,7 @@
+$(".hamburger .hamburger__inner").click(function(){
+    $(".wrapper").toggleClass("active")
+  })
+  
+  $(".top_navbar .fas").click(function(){
+     $(".profile_dd").toggleClass("active");
+  });
